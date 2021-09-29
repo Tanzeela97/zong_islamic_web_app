e2b98aacccf984b0c01813bb6bce379cb65a049d
syedfaran <syed_faran@live.com>
2021-09-29 05:11:22 -0700

    currrnet_detail_section

--- lib/src/ui/page/home_page/current_detail_section.dart
+++ lib/src/ui/page/home_page/current_detail_section.dart
@@ -7,24 +7,50 @@ class CurrentDetailSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 15),
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
       width: double.infinity,
-      height: 300,
-      decoration:  BoxDecoration(
-        color: Colors.grey[200]
-      ),
+      height: 250,
+      decoration: BoxDecoration(color: Colors.grey[200]),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children:  [
-          const Text('20 Safar 1443 AH, Sep 29 2021'),
-          const Text('Dhuhr: 12:22'),
-          const WidgetDivider(thickness: 5,endIndent: 120),
-          const Text('05:07 [H] 05:16 [J]'),
-          Row(
-            children: [
-              
-            ],
-          ),
+        children: [
+          const Spacer(flex: 2),
+          Text('20 Safar 1443 AH, Sep 29 2021',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyText1!
+                  .copyWith(fontSize: 18)),
+          Text('Dhuhr: 12:22',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyText1!
+                  .copyWith(fontSize: 28)),
+          const WidgetDivider(thickness: 3, endIndent: 100),
+          const _Row(string: 'Sehar',value: '05:07 [H] 05:16 [J]',),
+          const _Row(string: 'Aftar',value: '05:07 [H] 05:16 [J]',),
+          const Spacer(flex: 1),
+        ],
+      ),
+    );
+  }
+}
+
+class _Row extends StatelessWidget {
+  final String string;
+  final String value;
+  const _Row({Key? key,required this.string, required this.value}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 230,
+      child: Row(
+        children: [
+          Expanded(child: Text(string,style: Theme.of(context)
+              .textTheme
+              .bodyText1!
+              .copyWith(fontSize: 18))),
+          Expanded(flex: 2,child: Text(value)),
         ],
       ),
     );
