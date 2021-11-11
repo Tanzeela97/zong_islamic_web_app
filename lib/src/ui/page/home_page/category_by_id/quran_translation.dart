72f0f0033c5c0728bd970ddb2687ebad9693efb7
syedfaran <syed_faran@live.com>
2021-11-11 04:58:01 -0800

    surahWise implemented

--- lib/src/ui/page/home_page/category_by_id/quran_translation.dart
+++ lib/src/ui/page/home_page/category_by_id/quran_translation.dart
@@ -3,10 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:zong_islamic_web_app/src/cubit/cate_cubit/category_cubit.dart';
 import 'package:zong_islamic_web_app/src/cubit/cate_cubit/sub_category/quran_translation_cubit/quran_cubit.dart';
+import 'package:zong_islamic_web_app/src/cubit/surah_cubit/surah_cubit.dart';
 import 'package:zong_islamic_web_app/src/model/content_by_category_id.dart';
 import 'package:zong_islamic_web_app/src/model/news.dart';
+import 'package:zong_islamic_web_app/src/model/surah_wise.dart';
 import 'package:zong_islamic_web_app/src/model/trending.dart';
 import 'package:zong_islamic_web_app/src/resource/repository/cate_repository.dart';
+import 'package:zong_islamic_web_app/src/resource/repository/surah_repository.dart';
 import 'package:zong_islamic_web_app/src/resource/utility/app_colors.dart';
 import 'package:zong_islamic_web_app/src/resource/utility/app_string.dart';
 import 'package:zong_islamic_web_app/src/ui/widget/error_text.dart';
@@ -17,12 +20,14 @@ import 'package:zong_islamic_web_app/src/ui/widget/widget_empty_box.dart';
 import 'package:zong_islamic_web_app/src/ui/widget/widget_loading.dart';
 import 'package:zong_islamic_web_app/src/ui/widget/widget_video_tile.dart';
 
-enum TapOption { paraWise, SurahWise }
+enum TapOption { paraWise, surahWise }
 
 class QuranAndTranslation extends StatefulWidget {
   final String contendId;
   final String number;
-  const QuranAndTranslation(this.contendId,this.number, {Key? key}) : super(key: key);
+
+  const QuranAndTranslation(this.contendId, this.number, {Key? key})
+      : super(key: key);
 
   @override
   State<QuranAndTranslation> createState() => _QuranAndTranslationState();
@@ -31,12 +36,14 @@ class QuranAndTranslation extends StatefulWidget {
 class _QuranAndTranslationState extends State<QuranAndTranslation> {
   @override
   void initState() {
-    BlocProvider.of<CategoryCubit>(context).getCategoryById(widget.contendId,widget.number);
+    BlocProvider.of<CategoryCubit>(context)
+        .getCategoryById(widget.contendId, widget.number);
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    print('QuranAndTranslation');
     return Scaffold(
       body: Column(
         children: [
@@ -48,7 +55,9 @@ class _QuranAndTranslationState extends State<QuranAndTranslation> {
               } else if (state is CategoryLoadingState) {
                 return const WidgetLoading();
               } else if (state is CategorySuccessState) {
-                return _QuranTranslationByCategory(state.category!,widget.number,title: state.category!.title!);
+                return _QuranTranslationByCategory(
+                    state.category!, widget.number,
+                    title: state.category!.title!);
               } else if (state is CategoryErrorState) {
                 return const ErrorText();
               } else {
@@ -66,7 +75,10 @@ class _QuranTranslationByCategory extends StatefulWidget {
   final ContentByCateId cateId;
   final String number;
   final String title;
-  const _QuranTranslationByCategory(this.cateId,this.number, {Key? key,this.title= 'Islam'}) : super(key: key);
+
+  const _QuranTranslationByCategory(this.cateId, this.number,
+      {Key? key, this.title = 'Islam'})
+      : super(key: key);
 
   @override
   State<_QuranTranslationByCategory> createState() =>
@@ -76,28 +88,28 @@ class _QuranTranslationByCategory extends StatefulWidget {
 class _QuranTranslationByCategoryState
     extends State<_QuranTranslationByCategory>
     with SingleTickerProviderStateMixin {
-
   final _sizedBox = const SizedBox(height: 10);
   final quranCubit = QuranCubit(CategoryRepository.getInstance()!);
   late TabController tabController;
   bool absorb = false;
+
   @override
   void initState() {
-    quranCubit.getQuranTranslationById(widget.cateId.subMenu![0].catId!,widget.number);
+    quranCubit.getQuranTranslationById(
+        widget.cateId.subMenu![0].catId!, widget.number);
     tabController = TabController(length: 2, vsync: this);
-    tabController.addListener(() {
-      setState(() {});
-    });
+    tabController.addListener(() {});
     super.initState();
   }
 
   @override
   void dispose() {
     quranCubit.close();
+    tabController.dispose();
     super.dispose();
   }
 
-  void setAbsorb(bool value){
+  void setAbsorb(bool value) {
     setState(() {
       absorb = value;
     });
@@ -109,7 +121,7 @@ class _QuranTranslationByCategoryState
       length: widget.cateId.subMenu!.length,
       initialIndex: 0,
       child: Scaffold(
-        appBar:  WidgetAppBar(title: widget.title),
+        appBar: WidgetAppBar(title: widget.title),
         body: BlocProvider.value(
           value: quranCubit,
           child: Column(
@@ -147,7 +159,7 @@ class _QuranTranslationByCategoryState
                     labelColor: Colors.black,
                     onTap: (index) {
                       quranCubit.getQuranTranslationById(
-                          widget.cateId.subMenu![index].catId!,widget.number);
+                          widget.cateId.subMenu![index].catId!, widget.number);
                     },
                   ),
                 ),
@@ -159,14 +171,12 @@ class _QuranTranslationByCategoryState
                   if (state is QuranInitial) {
                     return const EmptySizedBox();
                   } else if (state is QuranLoadingState) {
-                    WidgetsBinding.instance!.addPostFrameCallback((_){
-
+                    WidgetsBinding.instance!.addPostFrameCallback((_) {
                       setAbsorb(true);
                     });
                     return const WidgetLoading();
                   } else if (state is QuranSuccessState) {
-                    WidgetsBinding.instance!.addPostFrameCallback((_){
-
+                    WidgetsBinding.instance!.addPostFrameCallback((_) {
                       setAbsorb(false);
                     });
                     if (state.category!.vod! is List) {
@@ -230,15 +240,19 @@ class _SurahWise extends StatefulWidget {
 
 class _SurahWiseState extends State<_SurahWise> {
   String? dropdownValue;
+  final surahCubit = SurahCubit(SurahRepository.getInstance()!);
 
   @override
   void initState() {
-    dropdownValue = widget.surah[0].itemName!;
+    dropdownValue = widget.surah.first.itemName!;
+    surahCubit.getSurahByIdAndLang(int.parse(widget.surah.first.id!));
+
     super.initState();
   }
 
   @override
   void dispose() {
+    surahCubit.close();
     super.dispose();
   }
 
@@ -269,6 +283,11 @@ class _SurahWiseState extends State<_SurahWise> {
                 items: widget.surah
                     .map<DropdownMenuItem<String>>((Trending value) {
                   return DropdownMenuItem<String>(
+                    onTap: () {
+                      if (widget.surah.contains(value)) {
+                        surahCubit.getSurahByIdAndLang(int.parse(value.id!));
+                      }
+                    },
                     value: value.itemName,
                     child: Text(value.itemName!),
                   );
@@ -277,7 +296,68 @@ class _SurahWiseState extends State<_SurahWise> {
             ],
           ),
         ),
+        BlocBuilder<SurahCubit, SurahState>(
+            bloc: surahCubit,
+            builder: (context, state) {
+              if (state is SurahInitial) {
+                return const EmptySizedBox();
+              } else if (state is SurahLoadingState) {
+                return const WidgetLoading();
+              } else if (state is SurahSuccessState) {
+                return _SurahListUi(state.arbiSurah, state.urduSurah);
+              } else if (state is SurahErrorState) {
+                return Text(state.message);
+              } else {
+                return const Text('failed');
+              }
+            }),
       ],
+    );
+  }
+}
+
+class _SurahListUi extends StatelessWidget {
+  final List<SurahWise> arabicList;
+  final List<SurahWise> urduList;
+
+  const _SurahListUi(this.arabicList, this.urduList, {Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        physics: const ClampingScrollPhysics(),
+          itemCount: arabicList.length,
+          itemBuilder: (context, index) {
+            return Container(
+              margin: const EdgeInsets.symmetric(horizontal: 12.0,vertical: 10),
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              decoration: BoxDecoration(
+                color: AppColor.lightGrey,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    arabicList[index].ar,
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline1!
+                        .copyWith(color: Colors.black),textAlign: TextAlign.center,
+                  ),
+                  Text(
+                    urduList[index].ur,
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline5!
+                        .copyWith(color: Colors.black),
+                      textAlign: TextAlign.center
+                  ),
+                ],
+              ),
+            );
+          }),
     );
   }
 }
