de26d356fc5102da277e8af0c96b381e947114fb
bilalidrees <bilalidrees@vectracom.com>
2022-03-18 14:50:24 +0500

    up

--- lib/SpeechScreen.dart
+++ lib/SpeechScreen.dart
@@ -83,50 +83,6 @@ class _VoiceCommandSearchState extends State<VoiceCommandSearch> {
         ),
       ),
     );
-    // return MaterialApp(
-    //   home: Scaffold(
-    //     body: AvatarGlow(
-    //       animate: _isListening,
-    //       glowColor: Theme.of(context).primaryColor,
-    //       endRadius: 75.0,
-    //       duration: const Duration(milliseconds: 2000),
-    //       repeatPauseDuration: const Duration(milliseconds: 100),
-    //       repeat: true,
-    //       child: FloatingActionButton(
-    //         onPressed: startListening,
-    //         child: Icon(_isListening ? Icons.mic : Icons.mic_none),
-    //       ),
-    //     ),
-    //     // Column(children: [
-    //     //   Container(
-    //     //     child: Column(
-    //     //       children: <Widget>[
-    //     //         AvatarGlow(
-    //     //           animate: _isListening,
-    //     //           glowColor: Theme.of(context).primaryColor,
-    //     //           endRadius: 75.0,
-    //     //           duration: const Duration(milliseconds: 2000),
-    //     //           repeatPauseDuration: const Duration(milliseconds: 100),
-    //     //           repeat: true,
-    //     //           child: FloatingActionButton(
-    //     //             onPressed: startListening,
-    //     //             child: Icon(_isListening ? Icons.mic : Icons.mic_none),
-    //     //           ),
-    //     //         ),
-    //     //       ],
-    //     //     ),
-    //     //   ),
-    //     //   Expanded(
-    //     //     flex: 4,
-    //     //     child: RecognitionResultsWidget(lastWords: lastWords, level: level),
-    //     //   ),
-    //     //   Expanded(
-    //     //     flex: 1,
-    //     //     child: ErrorWidget(lastError: lastError),
-    //     //   ),
-    //     // ]),
-    //   ),
-    // );
   }
 
   void startListening() {
@@ -158,69 +114,6 @@ class _VoiceCommandSearchState extends State<VoiceCommandSearch> {
   }
 }
 
-class RecognitionResultsWidget extends StatelessWidget {
-  const RecognitionResultsWidget({
-    Key? key,
-    required this.lastWords,
-    required this.level,
-  }) : super(key: key);
-
-  final String lastWords;
-  final double level;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Center(
-          child: Text(
-            'Recognized Words',
-            style: TextStyle(fontSize: 22.0),
-          ),
-        ),
-        Expanded(
-          child: Stack(
-            children: <Widget>[
-              Container(
-                color: Colors.amber,
-                child: Center(
-                  child: Text(
-                    lastWords,
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-}
 
 
-class ErrorWidget extends StatelessWidget {
-  const ErrorWidget({
-    Key? key,
-    required this.lastError,
-  }) : super(key: key);
 
-  final String lastError;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Center(
-          child: Text(
-            'Error Status',
-            style: TextStyle(fontSize: 22.0),
-          ),
-        ),
-        Center(
-          child: Text(lastError),
-        ),
-      ],
-    );
-  }
-}
